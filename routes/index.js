5d34fbe0448c9c4c1df515d7b39abf8c048e9cc5
Nicola Bortignon <info@nicolabortignon.com>
2015-06-09 15:45:49 +0100

    Rever to e0ef45e6823149dcd561e774fea8a8e934816854

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,19 @@
-var models  = require('../models');
+var models = require('../models');
 var express = require('express');
-<<<<<<< HEAD
 var router = express.Router();
 var fs = require('fs');
 var http = require('http');
 var url = require('url');
-=======
-var router  = express.Router();
->>>>>>> parent of 5ff435d... Create models for Artworks and Cities
 
 router.get('/', function(req, res) {
-  models.User.findAll({
-    include: [ models.Task ]
-  }).then(function(users) {
-    res.render('index', {
-      title: 'Express',
-      users: users
+    models.Artwork.findAll({
+        include: models.City
+    }).then(function(artworks) {
+        res.send(artworks);
+
     });
-  });
 });
 
-<<<<<<< HEAD
 router.get('/images/:imageURL', function(req, res) {
     var file = 'images/' + req.param('imageURL');
     fs.stat(file, function(err, stat) {
@@ -33,6 +26,3 @@ router.get('/images/:imageURL', function(req, res) {
 
 
 module.exports = router;
-=======
-module.exports = router;
->>>>>>> parent of 5ff435d... Create models for Artworks and Cities
