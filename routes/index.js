c61d71d40271cd21b469f6f6c126b0bb01565b0e
Nicola Bortignon <info@nicolabortignon.com>
2015-06-09 15:41:08 +0100

    Revert "Create models for Artworks and Cities"
    This reverts commit 5ff435d3c489a279bd2981da6011015af2475ae2.
    
    Conflicts:
    	routes/artworks.js
    	routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,26 @@
-var models = require('../models');
+var models  = require('../models');
 var express = require('express');
+<<<<<<< HEAD
 var router = express.Router();
 var fs = require('fs');
 var http = require('http');
 var url = require('url');
+=======
+var router  = express.Router();
+>>>>>>> parent of 5ff435d... Create models for Artworks and Cities
 
 router.get('/', function(req, res) {
-    models.Artwork.findAll({
-        include: models.City
-    }).then(function(artworks) {
-        res.send(artworks);
-
+  models.User.findAll({
+    include: [ models.Task ]
+  }).then(function(users) {
+    res.render('index', {
+      title: 'Express',
+      users: users
     });
+  });
 });
 
+<<<<<<< HEAD
 router.get('/images/:imageURL', function(req, res) {
     var file = 'images/' + req.param('imageURL');
     fs.stat(file, function(err, stat) {
@@ -26,3 +33,6 @@ router.get('/images/:imageURL', function(req, res) {
 
 
 module.exports = router;
+=======
+module.exports = router;
+>>>>>>> parent of 5ff435d... Create models for Artworks and Cities
